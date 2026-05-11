a4304d5de07dbc8e15822c63330a232b30914d8f
moskalev_andrew <moskalev.a.a.155@gmail.com>
2026-05-12 01:16:50 +0400

    fix: add CORS

--- src/main/java/com/team/cinema_app/config/SecurityConfig.java
+++ src/main/java/com/team/cinema_app/config/SecurityConfig.java
@@ -14,6 +14,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -29,6 +34,7 @@ public class SecurityConfig {
 
         http
 
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 // отключаем csrf для REST API
                 .csrf(csrf -> csrf.disable())
 
@@ -100,6 +106,26 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
             throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        config.setAllowedHeaders(List.of(
+                "Authorization",
+                "Content-Type"
+        ));
+
+        config.setExposedHeaders(List.of("Authorization"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 
 //    @Bean
--- src/main/java/com/team/cinema_app/model/Reservation.java
+++ src/main/java/com/team/cinema_app/model/Reservation.java
@@ -1,4 +1,51 @@
 package com.team.cinema_app.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Table(name = "reservations")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @NotNull
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "session_id")
+    @NotNull
+    private Session session;
+
+    @ManyToMany
+    @JoinTable(
+            name = "reservation_seats",
+
+            joinColumns = @JoinColumn(
+                    name = "reservation_id"
+            ),
+
+            inverseJoinColumns = @JoinColumn(
+                    name = "seat_id"
+            )
+    )
+    @NotNull
+    private List<Seat> seats = new ArrayList<>();;
+
+    @NotNull
+    private double price;
 }
