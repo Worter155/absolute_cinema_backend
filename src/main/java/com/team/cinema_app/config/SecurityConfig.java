348559b54bc84fb37cbb2196c404b624078d51ea
moskalev_andrew <moskalev.a.a.155@gmail.com>
2026-05-12 10:47:35 +0400

    fix: CORS

--- src/main/java/com/team/cinema_app/config/SecurityConfig.java
+++ src/main/java/com/team/cinema_app/config/SecurityConfig.java
@@ -112,7 +112,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "http://localhost:5173"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
         config.setAllowedHeaders(List.of(
